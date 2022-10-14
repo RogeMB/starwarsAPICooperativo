b7cd5db889ae4e998954b32e305b09897562b4e6
ProgramadorIV <antoniojz1998@gmail.com>
2022-10-14 14:25:23 +0200

    Development from species completed

--- starWarsAPICoop/src/app/components/species/species.component.ts
+++ starWarsAPICoop/src/app/components/species/species.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Specie } from 'src/app/interfaces/species';
+import { SpeciesService } from 'src/app/services/species.service';
 
 @Component({
   selector: 'app-species',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeciesComponent implements OnInit {
 
-  constructor() { }
+  speciesList: Specie[]=[];
+  pages: number = 0;
+
+  constructor(private specieService: SpeciesService) { }
 
   ngOnInit(): void {
+    this.getSpeciePage(1);
   }
+
+  getSpeciePage(id: number){
+
+    this.specieService.getSpecies(id).subscribe( resp => {
+      this.speciesList = resp.results;
+      this.pages = Math.ceil(resp.count / 10);
+    });
+  }
+
+  getPhotoUrl(specie: Specie){
+
+    let id = specie.url.split('/').reverse()[1];
+    return `https://starwars-visualguide.com/assets/img/species/${id}.jpg`
+  }
+
+  errorPhoto(){
+    return 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png'
+  }
+
+  counter(){
+    return new Array(this.pages);
+  }
+
+
 
 }
--- starWarsAPICoop/src/app/services/species.service.ts
+++ starWarsAPICoop/src/app/services/species.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { SpeciesResult } from '../interfaces/species';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpeciesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSpecies(page: number): Observable<SpeciesResult>{
+    return this.http.get<SpeciesResult>(`${environment.apiBaseUrl}/species?page=${page}`)
+  }
 }
